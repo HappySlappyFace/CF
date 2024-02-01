f843e536d9593ab940a56614b351cebfed11a73e
HappySlappyFace <11047926+HappySlappyFace@users.noreply.github.com>
2024-02-01 01:04:07 +0100

    section3 implementation and lazy loading Research

--- src/components/home.js
+++ src/components/home.js
@@ -2,7 +2,7 @@ import { $content, createHtmlElement } from "../index.js";
 import { registerButton } from "./registerButton.js";
 import "../styles/home.css";
 import Img1 from "../img/section1.png";
-
+import Img3 from "../img/section3.png";
 
 function importAll(r) {
   let imgs = {};
@@ -17,11 +17,10 @@ const images = importAll(require.context('../img/pastHackathons/', false, /\.(pn
 function render() {
   const $sections = createHtmlElement("div", null, ["sections"], null); 
   const $section1 = createHtmlElement("div", null, ["section1"], null);
-  
-  const $img = new Image();
-  $img.src = Img1;
-  $img.id = "section1Background";
-  $section1.appendChild($img);
+  const $img1 = new Image();
+  $img1.src = Img1;
+  $img1.id = "section1Background";
+  $section1.appendChild($img1);
 
   const $section1Content = createHtmlElement(
     "div",
@@ -45,10 +44,12 @@ function render() {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
   );
   $section1Content.appendChild($h3);
-
   $section1Content.appendChild(registerButton());
-
   $section1.appendChild($section1Content);
+
+
+  //section 2
+
 
   const $section2 = createHtmlElement("div", null, ["section2"], null);
   const $h2 = createHtmlElement("h2", null, null, "Past Hackathons");
@@ -59,9 +60,7 @@ function render() {
     ["section-slideshow"],
     null
   );
-  
   for (let key in images) {
-    
     if (images.hasOwnProperty(key)) {
       let imageUrl = images[key];
       const img = new Image();
@@ -70,18 +69,41 @@ function render() {
       $sectionSlideshow.appendChild(img);
     }
   }
-  
   $section2.appendChild($sectionSlideshow);
 
-  // const img2 = new Image();
-  // img2.src = Img2;
-  // img2.classList.add("indexSection2");
-  // $section2.appendChild(img2);
 
+  // section 3
+
+
+  const $section3 = createHtmlElement("div", null, ["section3"], null);
+
+  const $img3 = new Image();
+  $img3.src = Img3;
+  $img3.id = "section3Background";
+  $section3.appendChild($img3);
+  
+
+  const $section3Content = createHtmlElement(
+    "div",
+    null,
+    ["section3Content"],
+    null
+  );
+
+  const $h2Section3 = createHtmlElement("h2", "section3Title", null, "Sponsors and Trainers");
+
+  $section3Content.appendChild($h2Section3);
+
+
+
+
+  $section3.appendChild($section3Content);
   $sections.appendChild($section1);
   $sections.appendChild($section2);
+  $sections.appendChild($section3);
   $content.appendChild($sections);
   
 }
+
 
 export { render as createHome };
