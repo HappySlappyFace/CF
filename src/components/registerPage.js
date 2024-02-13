401aa75a7def7f02bbc965fb5552a9c33c22ba2f
HappySlappyFace <11047926+HappySlappyFace@users.noreply.github.com>
2024-02-13 01:23:06 +0100

    FormCSS&Tweaking/responsiveTweaking/mobileBgs

--- src/components/registerPage.js
+++ src/components/registerPage.js
@@ -1,6 +1,7 @@
 import { $content, createHtmlElement } from "../index.js";
 import "../styles/register.css";
 import Img3 from "../img/section3.png";
+
 function render() {
   const $sections = createHtmlElement("div", null, ["sections"], null);
   const $section1 = createHtmlElement("div", null, ["section1"], null);
@@ -16,13 +17,17 @@ function render() {
     ["registerContent"],
     null
   );
-  const $h1 = createHtmlElement("h1", "registerTitle", null, "CodeFiesta");
-  $registerContent.appendChild($h1);
+  const $Logo = new Image();
+  $Logo.src = require("../logos/LogoCF.svg");
+  $Logo.id = "Logo";
+  $Logo.alt = "CodeFiesta Logo";
+  $registerContent.appendChild($Logo);
+
   const $h3 = createHtmlElement(
     "h3",
-    "registerParagraph",
+    "registerTitle",
     null,
-    "Sign up for the hackathon!"
+    "¡24 Hour Hackathon!"
   );
   $registerContent.appendChild($h3);
 
@@ -52,9 +57,9 @@ function render() {
     null
   );
   $name.type = "text";
-  $name.placeholder = "Name";
+  $name.placeholder = "Enter Your Full Name";
   $name.name = "name";
-  $form.appendChild($name);
+
   const $phone = createHtmlElement(
     "input",
     "phone",
@@ -62,9 +67,9 @@ function render() {
     null
   );
   $phone.type = "tel";
-  $phone.placeholder = "Phone Number";
+  $phone.placeholder = "Enter Your Number";
   $phone.name = "phone";
-  $form.appendChild($phone);
+
   const $email = createHtmlElement(
     "input",
     "email",
@@ -72,9 +77,9 @@ function render() {
     null
   );
   $email.type = "email";
-  $email.placeholder = "Email";
+  $email.placeholder = "Enter Your Email";
   $email.name = "email";
-  $form.appendChild($email);
+
   const $university = createHtmlElement(
     "input",
     "university",
@@ -82,23 +87,9 @@ function render() {
     null
   );
   $university.type = "text";
-  $university.placeholder = "University";
+  $university.placeholder = "Enter Your University/Highschool name";
   $university.name = "university";
-  $form.appendChild($university);
-  const $education = createHtmlElement(
-    "select",
-    "education",
-    ["registerFormElement"],
-    null
-  );
-  const $option1 = createHtmlElement("option", null, null, "High School");
-  $education.appendChild($option1);
-  const $option2 = createHtmlElement("option", null, null, "Undergraduate");
-  $education.appendChild($option2);
-  const $option3 = createHtmlElement("option", null, null, "Graduate");
-  $education.appendChild($option3);
-  $education.name = "education";
-  $form.appendChild($education);
+
   const $team = createHtmlElement(
     "input",
     "team",
@@ -106,19 +97,19 @@ function render() {
     null
   );
   $team.type = "text";
-  $team.placeholder = "Team Name";
+  $team.placeholder = "Enter Your Team Name";
   $team.name = "team";
-  $form.appendChild($team);
+
   const $birthday = createHtmlElement(
     "input",
     "birthday",
     ["registerFormElement"],
     null
   );
-  $birthday.type = "date";
-  $birthday.placeholder = "YYYY-MM-DD";
+  $birthday.type = "text";
+  $birthday.placeholder = "Enter Your Date Of Birth";
   $birthday.name = "birthday";
-  $form.appendChild($birthday);
+
   const $submit = createHtmlElement(
     "input",
     "submit",
@@ -129,31 +120,19 @@ function render() {
   $submit.value = "Submit";
   $submit.onclick = validateRegisterForm;
 
-  //add phone number and a select with team members from 1 to 4
-
-  const $members = createHtmlElement(
-    "select",
-    "members",
-    ["registerFormElement"],
-    null
-  );
-  const $option4 = createHtmlElement("option", null, null, "1");
-  $members.appendChild($option4);
-  const $option5 = createHtmlElement("option", null, null, "2");
-  $members.appendChild($option5);
-  const $option6 = createHtmlElement("option", null, null, "3");
-  $members.appendChild($option6);
-  const $option7 = createHtmlElement("option", null, null, "4");
-  $members.appendChild($option7);
-  $members.name = "members";
-  $form.appendChild($members);
-
+  $form.appendChild($team);
+  $form.appendChild($name);
+  $form.appendChild($email);
+  $form.appendChild($phone);
+  $form.appendChild($university);
+  $form.appendChild($birthday);
   $form.appendChild($submit);
 
   $registerContent.appendChild($form);
   $section1.appendChild($registerContent);
   $content.appendChild($section1);
 }
+
 function validateRegisterForm() {
   // Retrieve form elements using querySelector
   const name = document.querySelector("#registerForm input[name='name']").value;
@@ -166,15 +145,9 @@ function validateRegisterForm() {
   const university = document.querySelector(
     "#registerForm input[name='university']"
   ).value;
-  const education = document.querySelector(
-    "#registerForm select[name='education']"
-  ).value;
   const team = document.querySelector("#registerForm input[name='team']").value;
   const birthday = document.querySelector(
     "#registerForm input[name='birthday']"
-  ).value;
-  const members = document.querySelector(
-    "#registerForm select[name='members']"
   ).value;
 
   // Validate each field
